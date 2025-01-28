2489c52c050ed91663041a3f7bf8ab9b3eec1f20
Maksim Burakov <maksim.burakou@gmail.com>
2025-01-28 15:21:47 -0500

    tauri: feat - open in background; feat - add tray icon show/hide switcher of left click

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,21 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 
 use tauri::{
+  Manager,
   menu::{Menu, MenuItem},
-  tray::TrayIconBuilder,
+  tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
 };
+
+use std::sync::Mutex;
+
+// Define the state
+struct WindowState(Mutex<bool>);
 
 pub fn run() {
   tauri::Builder::default()
     .setup(|app| {
+      // Initialize the window state
+      app.manage(WindowState(Mutex::new(false)));
       // Hide the dock icon on macOS
       #[cfg(target_os = "macos")]
       app.set_activation_policy(tauri::ActivationPolicy::Accessory);
@@ -39,6 +47,33 @@ pub fn run() {
         })
         // Tray icon
         .icon(app.default_window_icon().unwrap().clone())
+        .on_tray_icon_event(move |tray, event| match event {
+          TrayIconEvent::Click {
+              button: MouseButton::Left,
+              button_state: MouseButtonState::Up,
+              ..
+          } => {
+              let app = tray.app_handle();
+              let window_state = app.state::<WindowState>();
+              let mut is_visible = window_state.0.lock().unwrap();
+
+              if let Some(window) = app.get_webview_window("main") {
+                  if *is_visible {
+                      // Hide the window
+                      let _ = window.hide();
+                  } else {
+                      // Show and focus the window
+                      let _ = window.show();
+                      let _ = window.set_focus();
+                  }
+                  // Toggle the state
+                  *is_visible = !*is_visible;
+              }
+          }
+          _ => {
+              println!("unhandled event {event:?}");
+          }
+        })
         .build(app)?;
       Ok(())
     })
