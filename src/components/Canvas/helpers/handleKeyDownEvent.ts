534a53fe362c7c17bbd7778c599ae3f9ddbd491b
Maksim Burakov <maksim.burakou@gmail.com>
2025-02-06 11:48:35 -0500

    canvas: fix - redo and undo shortcuts bug

--- src/components/Canvas/helpers/handleKeyDownEvent.ts
+++ src/components/Canvas/helpers/handleKeyDownEvent.ts
@@ -4,23 +4,27 @@ export const handleKeyDownEvent = (
   event: KeyboardEvent,
   canvasRef: React.RefObject<ReactSketchCanvasRef>
 ) => {
-  if (event.metaKey && event.key === "z") {
-    event.preventDefault();
-    canvasRef.current?.undo();
-  }
-
   if (event.shiftKey && event.metaKey && event.key === "z") {
     event.preventDefault();
     canvasRef.current?.redo();
+    return;
+  }
+
+  if (event.metaKey && event.key === "z") {
+    event.preventDefault();
+    canvasRef.current?.undo();
+    return;
   }
 
   if (event.metaKey && event.key === "c") {
     event.preventDefault();
     canvasRef.current?.clearCanvas();
+    return;
   }
 
   if (event.metaKey && event.key === "r") {
     event.preventDefault();
     canvasRef.current?.resetCanvas();
+    return;
   }
 };
