de09de1b13a46d46bc473e563592f12945790d36
Maksim Burakov <maksim.burakou@gmail.com>
2025-02-12 19:49:01 -0500

    tauri: fix - build

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
     minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
-    outDir: "./dist/",
   },
   plugins: [react()],
 });
