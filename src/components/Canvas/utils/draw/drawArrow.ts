f3bbf7b317ae3cf7d711eb4ed5cf6ffb2ceb04d4
Maksim Burakov <maksim.burakou@gmail.com>
2025-04-05 12:34:39 -0400

    drawer: feat - improved arrow drawing

--- src/components/Canvas/utils/draw/drawArrow.ts
+++ src/components/Canvas/utils/draw/drawArrow.ts
@@ -20,43 +20,57 @@ export const drawArrow = (
     ? constrainLineToAxis(start, end, 15)
     : end;
 
+  const dx = adjustedEnd.x - start.x;
+  const dy = adjustedEnd.y - start.y;
+  const length = Math.sqrt(dx * dx + dy * dy);
+
   const options: Options = getRoughOptions({
     stroke: color,
     strokeWidth: thickness / 1.5,
     seed: drawableSeed,
   });
 
-  const arrowHeadLength = 15 + thickness * 2.5;
-  const angle = Math.atan2(adjustedEnd.y - start.y, adjustedEnd.x - start.x);
+  const angle = Math.atan2(dy, dx);
+
+  const baseArrowHeadLength = 15 + thickness * 2.5;
+
+  const minArrowLength = 15;
+
+  const arrowHeadLength =
+    length >= minArrowLength
+      ? Math.min(baseArrowHeadLength, length / 2)
+      : 0;
 
   const line = generateRoughShape(RoughShape.Line, start, adjustedEnd, options);
   if (line) roughCanvas.draw(line);
 
-  const arrowHead1: StrokePoint = {
-    x: adjustedEnd.x - arrowHeadLength * Math.cos(angle - Math.PI / 10),
-    y: adjustedEnd.y - arrowHeadLength * Math.sin(angle - Math.PI / 10),
-    pressure: adjustedEnd.pressure,
-  };
+  if (arrowHeadLength) {
+    const arrowHead1: StrokePoint = {
+      x: adjustedEnd.x - arrowHeadLength * Math.cos(angle - Math.PI / 10),
+      y: adjustedEnd.y - arrowHeadLength * Math.sin(angle - Math.PI / 10),
+      pressure: adjustedEnd.pressure,
+    };
 
-  const arrowHead2: StrokePoint = {
-    x: adjustedEnd.x - arrowHeadLength * Math.cos(angle + Math.PI / 10),
-    y: adjustedEnd.y - arrowHeadLength * Math.sin(angle + Math.PI / 10),
-    pressure: adjustedEnd.pressure,
-  };
+    const arrowHead2: StrokePoint = {
+      x: adjustedEnd.x - arrowHeadLength * Math.cos(angle + Math.PI / 10),
+      y: adjustedEnd.y - arrowHeadLength * Math.sin(angle + Math.PI / 10),
+      pressure: adjustedEnd.pressure,
+    };
 
-  const roughArrowHead1 = generateRoughShape(
-    RoughShape.Line,
-    adjustedEnd,
-    arrowHead1,
-    options
-  );
-  const roughArrowHead2 = generateRoughShape(
-    RoughShape.Line,
-    adjustedEnd,
-    arrowHead2,
-    options
-  );
+    const roughArrowHead1 = generateRoughShape(
+      RoughShape.Line,
+      adjustedEnd,
+      arrowHead1,
+      options
+    );
+    const roughArrowHead2 = generateRoughShape(
+      RoughShape.Line,
+      adjustedEnd,
+      arrowHead2,
+      options
+    );
 
-  if (roughArrowHead1) roughCanvas.draw(roughArrowHead1);
-  if (roughArrowHead2) roughCanvas.draw(roughArrowHead2);
+    if (roughArrowHead1) roughCanvas.draw(roughArrowHead1);
+    if (roughArrowHead2) roughCanvas.draw(roughArrowHead2);
+  }
 };
