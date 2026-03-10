e3f6ebea95a7c00bfe2a621bdeb51c247f184853
Maksim Burakov <maksim.burakou@gmail.com>
2026-03-10 14:06:07 -0400

    master: canvas rerender caused by useSelectedStrokes hook

--- src/components/Canvas/hooks/useSelectedStrokes.ts
+++ src/components/Canvas/hooks/useSelectedStrokes.ts
@@ -1,19 +1,26 @@
-import { usePresent, useShapeEditorStore, useTool } from "@/store";
+import { useShapeEditorStore, useTool } from "@/store";
+import { useHistoryStore } from "@/store/useHistoryStore";
 import { useShallow } from "zustand/react/shallow";
 import { getSelectedStrokes } from "../helpers/selectionSettings";
+import { useMemo } from "react";
+import { Tool } from "@/types";
 
 export const useSelectedStrokes = () => {
   const activeTool = useTool();
-  const present = usePresent();
   const { selectedStrokeIds } = useShapeEditorStore(
     useShallow((state) => ({
       selectedStrokeIds: state.selectedStrokeIds,
     })),
   );
 
-  return getSelectedStrokes({
-    activeTool,
-    selectedStrokeIds,
-    present,
-  });
+  return useMemo(() => {
+    if (activeTool !== Tool.Select || selectedStrokeIds.length === 0) return [];
+    
+    const { present } = useHistoryStore.getState();
+    return getSelectedStrokes({
+      activeTool,
+      selectedStrokeIds,
+      present,
+    });
+  }, [activeTool, selectedStrokeIds]);
 };
