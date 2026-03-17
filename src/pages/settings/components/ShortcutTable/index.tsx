3a21d838f8b50054b121b80d2d3641d77da7c140
Maksim Burakov <maksim.burakou@gmail.com>
2026-03-16 21:19:15 -0400

    master: feat - new settings page look

--- src/pages/settings/components/ShortcutTable/index.tsx
+++ src/pages/settings/components/ShortcutTable/index.tsx
@@ -11,14 +11,6 @@ export const ShortcutTable = ({ scope, actions }: ShortcutTableProps) => {
   return (
     <div className="shortcut-table__wrap">
       <table className="shortcut-table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Shortcut</th>
-            <th scope="col">Record</th>
-            <th scope="col">Reset</th>
-          </tr>
-        </thead>
         <tbody>
           {actions.map((action) => (
             <ShortcutRow
